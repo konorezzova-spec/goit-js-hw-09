8e4d327df80bd1a42628201f84f4e1824576a239
konorezzova-spec <konorezzova@gmail.com>
2026-05-17 13:42:12 +0300

    i finally got it

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,25 +2,27 @@ const form = document.querySelector('.feedback-form');
 const formEmail = form.elements.email;
 const formMessage = form.elements.message;
 const key = 'feedback-form-state';
-const formData = new FormData();
-const formDataObj = {
-    email: formData.get('email') || '',
-    message: formData.get('message') || '',
+
+const formDataObj = new FormData(form);
+const formData = {
+    email: formDataObj.get('email') || '',
+    message: formDataObj.get('message') || '',
 };
+
 const savedData = localStorage.getItem(key);
 
 if (savedData !== null) {
-    formDataObj.email = JSON.parse(savedData).email;
-    formDataObj.message = JSON.parse(savedData).message;
-    formEmail.value = formDataObj.email;
-    formMessage.value = formDataObj.message;
+    formData.email = JSON.parse(savedData).email;
+    formData.message = JSON.parse(savedData).message;
+    formEmail.value = formData.email;
+    formMessage.value = formData.message;
 }
 
 form.addEventListener('input', () => {
-    formDataObj.email = formEmail.value.trim();
-    formDataObj.message = formMessage.value.trim();
+    formData.email = formEmail.value.trim();
+    formData.message = formMessage.value.trim();
     
-    localStorage.setItem(key, JSON.stringify(formDataObj));
+    localStorage.setItem(key, JSON.stringify(formData));
 })
 
 form.addEventListener('submit', (evt) => {
@@ -29,10 +31,10 @@ form.addEventListener('submit', (evt) => {
         alert('Please fill in all the fields!');
         return;
     }
-    console.log(`formDataObj:`, formDataObj);
+    console.log(`formData:`, formData);
     // Clear form and localStorage
-    formDataObj.email = '';
-    formDataObj.message = '';
+    formData.email = '';
+    formData.message = '';
     localStorage.removeItem(key);
     form.reset();
 });
