c1ef100972a585f243f84c6e7a03e8557f751006
konorezzova-spec <konorezzova@gmail.com>
2026-05-17 06:16:34 +0300

    fixed form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,19 +1,24 @@
 const form = document.querySelector('.feedback-form');
-const emailInput = form.elements.email;
-const messageInput = form.elements.message;
+const formEmail = form.elements.email;
+const formMessage = form.elements.message;
 const key = 'feedback-form-state';
 
-emailInput.value = localStorage.getItem(key) ?? '';
-messageInput.value = localStorage.getItem(key) ?? '';
+formEmail.value = JSON.parse(localStorage.getItem(key)).email ?? '';
+formMessage.value = JSON.parse(localStorage.getItem(key)).message ?? '';
 
-form.addEventListener('input', (evt) => {
-    localStorage.setItem(key, evt.target.value);
+form.addEventListener('input', () => {
+    const inputs = new FormData(form);
+    const dataForm = {
+        email: inputs.get('email'),
+        message: inputs.get('message'),
+    };
+    localStorage.setItem(key, JSON.stringify(dataForm));
 })
 
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    console.log(`email:`, emailInput.value);
-    console.log(`message:`, messageInput.value);
+    console.log(`email:`, formEmail.value);
+    console.log(`message:`, formMessage.value);
     localStorage.removeItem(key);
     form.reset();
 });
