cbca4dd5300dbb107c7d098c7185286b63064774
konorezzova-spec <konorezzova@gmail.com>
2026-05-17 06:41:49 +0300

    fixed form: added chech on null and trim

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,14 +3,21 @@ const formEmail = form.elements.email;
 const formMessage = form.elements.message;
 const key = 'feedback-form-state';
 
-formEmail.value = JSON.parse(localStorage.getItem(key)).email ?? '';
-formMessage.value = JSON.parse(localStorage.getItem(key)).message ?? '';
+if (localStorage.getItem(key) !== null) {
+
+    formEmail.value = JSON.parse(localStorage.getItem(key)).email;
+    formMessage.value = JSON.parse(localStorage.getItem(key)).message;
+    
+} else {
+    formEmail.value = '';
+    formMessage.value = '';
+}
 
 form.addEventListener('input', () => {
     const inputs = new FormData(form);
     const dataForm = {
-        email: inputs.get('email'),
-        message: inputs.get('message'),
+        email: inputs.get('email').trim(),
+        message: inputs.get('message').trim(),
     };
     localStorage.setItem(key, JSON.stringify(dataForm));
 })
